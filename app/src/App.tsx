dbdc962b76cf011af93253d7165aaa66f3bfaf52
A1iHassan <ali012wkout@gmail.com>
2026-05-17 18:51:37 +0300

    testing barcode value

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef } from 'react'
+import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [barcodeValue, setBarcodeValue] = useState("")
   const videoRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
@@ -24,10 +26,34 @@ function App() {
     startStream()
   }, [])
 
+  const codeReader = new BrowserMultiFormatReader();
+
+  const videoElementId = 'video-stream';
+
+  // null deviceId defaults to the primary outward-facing camera
+  const deviceId = null;
+
+  codeReader.decodeFromVideoDevice(deviceId, videoElementId, (result, error) => {
+    if (result) {
+      // Successful capture
+      setBarcodeValue(result.getText())
+      console.log('Barcode Format:', result.getBarcodeFormat());
+
+      // Terminate active stream and decoding loop
+      codeReader.reset();
+    }
+
+    // Ignore typical Not Found errors during active scanning frames
+    if (error && !(error instanceof NotFoundException)) {
+      console.error('Processing error:', error);
+    }
+  })
+
   return (
     <div className="page">
       hello
       <video ref={videoRef} autoPlay playsInline muted></video>
+      {barcodeValue}
     </div>
   )
 }
